3bfbeab744f9eab8a6d80f2cd78a3cde5f61f826
tanjina akter <dali0023@algonquinlive.com>
2022-10-26 02:23:43 -0400

    added router to the project

--- src/App.js
+++ src/App.js
@@ -1,34 +1,20 @@
+import { Routes, Route, Link, Outlet } from "react-router-dom";
+import Home from "./routes/home/Home.component.jsx";
+import Navigation from "./routes/navigation/Navigation.component.jsx";
 
-import Directory from "./components/directory/directory.component.jsx";
+function Shop() {
+  return <h1>I am from Shop Page!</h1>;
+}
+
 function App() {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-  return <Directory categories={categories} />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
